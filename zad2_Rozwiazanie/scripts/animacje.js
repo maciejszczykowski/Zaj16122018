7b5e7ffc44ff9fdf8f243b872645bd5ea1ffd4c3
Maciej Szczykowski <maciej.szczykowski@gmail.com>
2018-12-16 12:30:57 +0100

    Poczatek

---
+++ zad2_Rozwiazanie/scripts/animacje.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $("#news").load("aktualnosci.html");
+
+    $("div.answer").css("display", "none");
+    $("div.questionAnswer > h1").click(function () {
+        $(this).next(".answer").slideToggle();
+    });
+
+    $('.fadein img:gt(0)').hide();
+    setInterval(function () {
+        $('.fadein :first-child').fadeOut()
+           .next('img').fadeIn()
+           .end().appendTo('.fadein');
+    },
+      3000);
+});
